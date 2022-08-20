7149c7541262b911dc9b800f53f377011bb92e97
RodrigoRPessanha <rodrigorospe@outlook.com>
2022-08-20 16:55:47 -0300

    EDAtividade08

--- Encontro08/src/EDAtividade08.java
+++ Encontro08/src/EDAtividade08.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EDEncontro08 {
+public class EDAtividade08 {
 
 	public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
