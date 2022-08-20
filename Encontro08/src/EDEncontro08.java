9befab91fa71f307d40e883abb176dd870646299
RodrigoRPessanha <rodrigorospe@outlook.com>
2022-08-20 15:42:51 -0300

    ADD EDEncontro08

---
+++ Encontro08/src/EDEncontro08.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EDEncontro08 {
+
+	public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Qual a altura da árvore desejada?");
+        int h = teclado.nextInt();
+		System.out.print("Resultado: "+nosNecessariosArvoreAVL(h));
+	}
+	
+	static long nosNecessariosArvoreAVL(int h) {
+		if(h == 0) {
+			return 0;
+		}else if (h == 1) {
+			return 1;
+		}
+		else {
+			return 1 + nosNecessariosArvoreAVL(h-1) + nosNecessariosArvoreAVL(h-2);
+		} 	
+	}	
+}
